30f915be344161d2c91e6a14d282968e61cc18e0
Rafael Meireles <rafameireles2011@gmail.com>
2021-11-18 07:42:01 -0300

    Setting back-end parameters

--- devpro/shortener/models.py
+++ devpro/shortener/models.py
@@ -11,3 +11,13 @@ class UrlRedirect(models.Model):
 
     def __str__(self):
         return f'UrlRedirect para {self.destiny}'
+
+
+class UrlLog(models.Model):
+    created_in = models.DateTimeField(auto_now_add=True)
+    origin = models.URLField(max_length=512, null=True, blank=True)
+    user_agent = models.CharField(max_length=512, null=True, blank=True)
+    host = models.CharField(max_length=512, null=True, blank=True)
+    ip = models.IPAddressField(null=True, blank=True)
+    url_redirect = models.ForeignKey(UrlRedirect, models.DO_NOTHING, related_name='logs')
+
