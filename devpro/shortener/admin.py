b22a290035f92ae07b6589a380f74fa84d6f0409
Rafael Meireles <rafameireles2011@gmail.com>
2021-11-19 19:12:49 -0300

    Setting back-end parameters

--- devpro/shortener/admin.py
+++ devpro/shortener/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from devpro.shortener.models import UrlRedirect
+from devpro.shortener.models import UrlRedirect, UrlLog
 
 
 # Register your models here.
@@ -8,3 +8,17 @@ from devpro.shortener.models import UrlRedirect
 @admin.register(UrlRedirect)
 class UrlRedirectAdmin(admin.ModelAdmin):
     list_display = ('destiny', 'slug', 'created_in', 'update_in',)
+
+
+@admin.register(UrlLog)
+class UrlLogAdmin(admin.ModelAdmin):
+    list_display = ('origin', 'created_in', 'user_agent', 'host', 'ip', 'url_redirect')
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
